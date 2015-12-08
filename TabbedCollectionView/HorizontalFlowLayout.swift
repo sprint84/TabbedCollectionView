804ae641dd4d5a2bc48b26254c6c78e55058d3bf
Guilherme Moura <guime.sprint@gmail.com>
2015-12-08 17:35:57 -0300

    wip

--- TabbedCollectionView/HorizontalFlowLayout.swift
+++ TabbedCollectionView/HorizontalFlowLayout.swift
@@ -9,62 +9,42 @@
 import UIKit
 
 class HorizontalFlowLayout: UICollectionViewFlowLayout {
-    var nbColumns: Int
-    var nbLines: Int
     override init() {
-        nbColumns = -1
-        nbLines = -1
         super.init()
-        self.scrollDirection = .Horizontal
-        self.minimumInteritemSpacing = 0
-        self.minimumLineSpacing = 0
+        scrollDirection = .Horizontal
+        minimumLineSpacing = 0
+        minimumInteritemSpacing = 0
     }
-    
-    required init?(coder aDecoder: NSCoder) {
-        nbColumns = -1
-        nbLines = -1
-        super.init(coder: aDecoder)
-        self.scrollDirection = .Horizontal
-        self.minimumInteritemSpacing = 0
-        self.minimumLineSpacing = 0
-    }
-    
-    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-        let columns = self.nbColumns != -1 ? self.nbColumns : Int(self.collectionView!.frame.size.width/self.itemSize.width)
-        let lines = self.nbLines != -1 ? self.nbLines : Int(self.collectionView!.frame.size.height/self.itemSize.height)
-        let idxPage = indexPath.row/(columns * lines)
-        
-        let L = indexPath.row - (idxPage * columns * lines)
-        
-        let xD = L / columns
-        let yD = L % columns
-        
-        let D = xD + yD * lines + idxPage * columns * lines
-        let fakeIndexPath = NSIndexPath(forItem: D, inSection: indexPath.section)
 
-        let attributes = super.layoutAttributesForItemAtIndexPath(fakeIndexPath)
-        return attributes
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        scrollDirection = .Horizontal
+        minimumLineSpacing = 0
+        minimumInteritemSpacing = 0
     }
     
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let newX = min(0, rect.origin.x - rect.size.width/2)
-        let newWidth = rect.size.width * 2 + (rect.origin.x - newX)
+        let allAttributes = super.layoutAttributesForElementsInRect(rect)
+        let verticalItemsCount = Int(floor(self.collectionView!.bounds.size.height / self.itemSize.height))
+        let horizontalItemsCount = Int(floor(self.collectionView!.bounds.size.width / self.itemSize.width))
+        let itemsPerPage = verticalItemsCount * horizontalItemsCount
         
-        let newRect = CGRect(x: newX, y: rect.origin.y, width: newWidth, height: rect.size.height)
-        
-        let allAttributesInRect = super.layoutAttributesForElementsInRect(newRect)
         var newAttributesInRect = [UICollectionViewLayoutAttributes]()
-        for attr in allAttributesInRect! {
-            let newAttr = self.layoutAttributesForItemAtIndexPath(attr.indexPath)
-            let nattr = attr.copy() as! UICollectionViewLayoutAttributes
-            nattr.frame = newAttr!.frame
-            nattr.center = newAttr!.center
-            nattr.bounds = newAttr!.bounds
-            nattr.hidden = newAttr!.hidden
-            nattr.size = newAttr!.size
+        for var i = 0; i < allAttributes!.count; i++ {
+            let attributes = allAttributes![i]
+            let nattr = attributes.copy() as! UICollectionViewLayoutAttributes
+            let currentPage = Int(floor(Double(i) / Double(itemsPerPage)))
+            let currentRow = Int(floor(Double(i - currentPage * itemsPerPage) / Double(horizontalItemsCount)))
+            let currentColumn = i % horizontalItemsCount
+            var frame = attributes.frame
+            frame.origin.x = self.itemSize.width * CGFloat(currentColumn) + CGFloat(currentPage) * self.collectionView!.bounds.size.width
+            frame.origin.y = self.itemSize.height * CGFloat(currentRow)
+            nattr.frame = frame
             newAttributesInRect.append(nattr)
+            if currentColumn == 3 && currentRow == 2 {
+                print(frame)
+            }
         }
-        
         return newAttributesInRect
     }
     
